4ab1faea9f5b18a7bd6fd56e0fbe97a7b9594de5
manumac86 <emmartinez86@gmail.com>
2025-06-15 16:48:37 +0200

    chore(vercel-insights): add Vercel Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { CartProvider } from "components/cart/cart-context";
 import { Navbar } from "components/layout/navbar";
 import { WelcomeToast } from "components/welcome-toast";
@@ -40,6 +41,7 @@ export default async function RootLayout({
   return (
     <html lang="en" className={`${bonanovaSC.variable} ${GeistSans.variable}`}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+        <SpeedInsights />
         <video
           src="/assets/portia-bg-video.mp4"
           autoPlay
