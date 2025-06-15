4e60c31051c785f82de7143fce279dedafcf5819
manumac86 <emmartinez86@gmail.com>
2025-06-15 17:48:32 +0200

    chore(flags): setup feature flags

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,23 @@
-export default {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   experimental: {
     ppr: true,
     inlineCss: true,
-    useCache: true
+    useCache: true,
   },
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "/s/files/**",
+      },
+    ],
+  },
 };
+
+const withVercelToolbar = require("@vercel/toolbar/plugins/next")();
+// Instead of module.exports = nextConfig, do this:
+module.exports = withVercelToolbar(nextConfig);
